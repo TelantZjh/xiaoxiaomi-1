5796ca8d3957023cc8be3d81279a189e0c89a127
mumu <zengmumu@126.com>
2020-03-24 11:19:30 +0800

    分类单击滚动与高亮

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -4,8 +4,13 @@
         <van-icon name="search" slot="right" />
      </van-nav-bar>
      <div class="content">
-         <div class="left" ref="left">
-             <div class="item" v-for="(item,index) in cate"  :key="index">
+         <div class="left"  ref="left">
+             <div class="item" 
+             v-for="(item,index) in cate"  
+             :key="index"
+             :class="{'active':index==current}"
+             @click="changeCur(index)"
+             >
                  {{item.category_name}}
             </div>
          </div>
@@ -17,13 +22,22 @@
 export default {
     data(){ return {
         cate:[],//分类信息
-        current:0,
+        current:0,// 当前被选择的item
+       
     }},
     created(){
         this.getCate();
     },
     methods:{
-       
+        changeCur(index){
+            this.current = index;
+            let left = this.$refs.left; //选择到left
+            let items = document.querySelectorAll(".left .item"); //选择到items
+            let el = items[index] //当前被单击的item
+            let leftH = left.offsetHeight;//left 高
+            left.scrollTop = el.offsetTop-leftH/2-el.offsetHeight/2;
+            // 设置left顶部滚动的高是=当前单击元素与left顶部的距离-left高度一般-单击元素自己高度一般
+        },
         // 获取分类
         getCate(){
             this.$http.get("/mi/category.php")
@@ -39,10 +53,16 @@ export default {
 }
 </script>
 <style scoped>
-    .item.active{color:#f30;}
+     .item.active{color:#f30}
+     /* 元素即拥有item 也拥有 active  class类名*/
     .category{ display: flex; flex-direction: column;  }
     .content{ flex:1; /* 自适应高 */ display: flex; height: 100%; overflow:hidden; }
-    .left{  width:1.5rem; height: 100%; overflow: auto; border-right: 1px solid #fafafa; /*宽1.5rem 高100%； 超出高度隐藏 有边框*/ }
+    .left{  width:1.5rem; height: 100%; overflow: auto; border-right: 1px solid #fafafa; /*宽1.5rem 高100%； 超出高度隐藏 有边框*/ 
+    scroll-behavior: smooth;
+    /* left滚动平滑 */
+    }
+    .left::-webkit-scrollbar{ display:none;}
+    /* left 滚动条隐藏 */
     
     .right{ flex:1; /* 自适应宽 */ height: 100%; overflow: auto; }
     .left .item{
